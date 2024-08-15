f2be2714754a1b66312c200edfc891f4246de39d
ElliotHarrison <ellioth1029@gmail.com>
2024-08-15 14:56:47 -0500

    restores post method to auth and creates a middleware dir

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,38 +11,31 @@ router.get("/signup", function (req, res, next) {
     return res.render("auth/signup");
 });
 
-// create user
-// POST /register
-router.post("/login", async function (req, res, next) {
-    console.log("this route is working...");
+router.post("/signup", async function (req, res, next) {
     try {
-        // check if user exists in database by email
-        const foundUser = await User.findOne({email: req.body.email});
-        // - if user does not exist, redirect to register
-        if (!foundUser) {
-            return res.redirect("/signup");
+        // check if user laready exists
+        // .exists returns true or false for whether something exists
+        const userDoesExist = await User.exists({email: req.body.email});
+        // - if they do exist, redirect to login
+        if (userDoesExist) {
+            return res.redirect("/login");
         }
-        // - if user does exists
-        // -- grab the user's hashed password and verify it against the given password
-        const match = await bcrypt.compare(req.body.password, foundUser.password);
-        // --- if no match -> tell the user password/email ivalid
-        if (!match) {
-            return res.render("auth/login");
-        }
-        // --- if match -> issue cookie
-        // add the user's authentication to the cookie
-        req.session.currentUser = {
-            id: foundUser._id,
-            username: foundUser.username,
-        };
-        // redirect to /
-        return res.redirect("/");
+        // - if they do not exist, we will create a new user and then redirect to login
+        // -- modify the req.body data to hash and salt the password
+        // salt -> generates a random addition to the table for increased hash complexity
+        const salt = await bcrypt.genSalt(10);
+        // hash -> convert normal string into a "random" string
+        const hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
+        await User.create(req.body);
+        return res.redirect("/login");
     } catch (error) {
         console.log(error);
         req.error = error;
         return next();
     }
 });
+// create user
 
 // login form
 // GET /login
